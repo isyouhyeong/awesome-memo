7fa5fd27a34318ad98d948579fe12d0b0c2b3ecb
aiden_Lee <isyouh77@gmail.com>
2024-05-27 23:36:16 +0900

    memo{셍상}
    생성

---
+++ static/app.js
@@ -0,0 +1,23 @@
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date(),
+      content: value,
+    }),
+  });
+  const jsonRes = await res.json();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = doucument.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
